ce8b923219aa42e2d9bb406f6f016811674dbfc0
Ainzerhald <89225940713@mail.ru>
2020-11-12 01:29:13 +0300

    Готовый рабочий прототип

--- app/src/main/java/com/example/prototype/MainActivity.kt
+++ app/src/main/java/com/example/prototype/MainActivity.kt
@@ -4,7 +4,9 @@ import android.database.SQLException
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.io.IOException
 
@@ -12,6 +14,8 @@ class MainActivity : AppCompatActivity() {
 
     private var mDBHelper: DatabaseHelper? = null
     private var mDb: SQLiteDatabase? = null
+
+    var players: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,10 +43,23 @@ class MainActivity : AppCompatActivity() {
             cursor.moveToNext()
         }
         cursor.close()
-        findViewById<TextView>(R.id.textView).setText(product);
+        var size = players.size - 1
+        findViewById<TextView>(R.id.player).setText(players.get((0..size).random()).toString());
+        findViewById<TextView>(R.id.quest).setText(product);
     }
 
     fun start(view: View){
-        setContentView(R.layout.game)
+        if(players.size > 1) {
+            setContentView(R.layout.game)
+        }
+        else{
+            Toast.makeText(this, "Минимум 2 игрока", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun add(view: View){
+        players.add(findViewById<EditText>(R.id.person).getText().toString())
+        Toast.makeText(this, "Игрок " + findViewById<EditText>(R.id.person).getText().toString() + " добавлен", Toast.LENGTH_SHORT).show()
+        findViewById<EditText>(R.id.person).setText("")
     }
 }
